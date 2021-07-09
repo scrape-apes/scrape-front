d8a5c25b10010da246d2ecf8ba89bbb4b12c5434
Chase Abbott <75389857+chase-abbott@users.noreply.github.com>
2021-07-09 09:02:57 -0700

    Api fetch (#2)
    * package installs
    
    * Auth page init
    
    * moved styles to a module
    
    * auth, search page created
    
    * package.json
    
    * grabs data from server and transferred over to details page
    
    * Add comments to search page
    
    * Add comments to app & auth cleanup unused imports
    
    * add state changes for username and password inputs
    
    Co-authored-by: NickDayFSD <Nick.Day.FSD@gmail.com>
    Co-authored-by: Joseph Davis <joethebartenderpdx@gmail.com>

--- App.js
+++ App.js
@@ -4,19 +4,28 @@ import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import Auth from './lib/auth.js';
 import Search from './lib/search.js';
+import Details from './lib/details.js';
+
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
+
+    // Defines the navigable components of the application
     <NavigationContainer>
       <Stack.Navigator>
+        {/* Each Stack.Screen creates a new page that we can route to, defines the Title of the Page and which compoenent it is */}
         <Stack.Screen
           name="Login/Sign up"
           component={Auth} />
         <Stack.Screen
           name="Search"
           component={Search} />
+        <Stack.Screen
+          name="Details"
+          component={Details} />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
