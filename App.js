1f5ab8a987160f6176c733ad35aadfa66edb6bc9
Chase <cabbott93@gmail.com>
2021-07-15 10:56:48 -0700

    frame up tabbing

--- App.js
+++ App.js
@@ -2,32 +2,23 @@ import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import React, { useState } from 'react';
-
-import Auth from './lib/auth.js';
-import Search from './lib/search.js';
-import Details from './lib/details.js';
+import Auth from './lib/auth.js'
+import Home from './lib/home.js';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-
   return (
-
     // Defines the navigable components of the application
     <NavigationContainer>
-
       <Stack.Navigator>
         {/* Each Stack.Screen creates a new page that we can route to, defines the Title of the Page and which component it is */}
         <Stack.Screen
           name="Login/Sign up"
           component={Auth} />
         <Stack.Screen
-          name="Search"
-          component={Search} />
-        <Stack.Screen
-          name="Details"
-          component={Details} />
-
+          name="Home"
+          component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
