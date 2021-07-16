06ee0bd2dd479e95643eb0b0b563fcb78a635114
Chase Abbott <75389857+chase-abbott@users.noreply.github.com>
2021-07-16 10:27:17 -0700

    Component refactor (#12)
    * location permissions and basic info
    
    * succesfully got location city
    
    * Need to test location services with given route on backend
    
    * rendered list of search items, provided pressable link and began styling
    
    * spelling fix
    
    * removed location services-in-progress
    
    * basic detail styling and onLongPress added
    
    * import organization
    
    * loader started
    
    * still handing off to companion
    
    * same
    
    * refactor login/signup into functions
    
    * added loader to search page
    
    * button style refactor
    
    * switch to heroku, functioning on iPad
    
    * Added styling
    
    * fixed bugs
    
    * move locations to auth
    
    * working location moved from search to auth
    
    * Multiple searches (#13)
    
    * start framing up multiple searches
    
    * stash
    
    * Multiple searches v1 (#11)
    
    * Loading bugfix (#9)
    
    * location permissions and basic info
    
    * succesfully got location city
    
    * Need to test location services with given route on backend
    
    * rendered list of search items, provided pressable link and began styling
    
    * spelling fix
    
    * removed location services-in-progress
    
    * basic detail styling and onLongPress added
    
    * import organization
    
    * loader started
    
    * still handing off to companion
    
    * same
    
    * refactor login/signup into functions
    
    * added loader to search page
    
    * added loader to auth page
    
    * added username and password error handling
    
    * remove comment
    
    * fixed bug where loader missing from android
    
    Co-authored-by: Gabriel Simek <gabrielsimek@outlook.com>
    Co-authored-by: Joseph Davis <joethebartenderpdx@gmail.com>
    Co-authored-by: David Delgadillo <daviddel.731@gmail.com>
    Co-authored-by: Chase <cabbott93@gmail.com>
    
    * change useState import
    
    * Added styling
    
    * fixed bugs
    
    * frame up tabbing
    
    * navigation attempted with errors
    
    * passing usrId successfully into other components
    
    * remove space
    
    Co-authored-by: NickDayFSD <80706339+NickDayFSD@users.noreply.github.com>
    Co-authored-by: Gabriel Simek <gabrielsimek@outlook.com>
    Co-authored-by: Joseph Davis <joethebartenderpdx@gmail.com>
    Co-authored-by: David Delgadillo <daviddel.731@gmail.com>
    
    * stash
    
    * move useEfffect above if statement in details.js
    
    * remove headers, add logout button, final draft
    
    Co-authored-by: NickDayFSD <80706339+NickDayFSD@users.noreply.github.com>
    Co-authored-by: Gabriel Simek <gabrielsimek@outlook.com>
    Co-authored-by: Joseph Davis <joethebartenderpdx@gmail.com>
    Co-authored-by: David Delgadillo <daviddel.731@gmail.com>
    
    * touched up final draft
    
    * remove save search button
    
    * add location back in
    
    Co-authored-by: Gabriel Simek <gabrielsimek@outlook.com>
    Co-authored-by: Joseph Davis <joethebartenderpdx@gmail.com>
    Co-authored-by: David Delgadillo <daviddel.731@gmail.com>
    Co-authored-by: NickDayFSD <Nick.Day.FSD@gmail.com>
    Co-authored-by: NickDayFSD <80706339+NickDayFSD@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,35 +1,26 @@
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import React, { useState } from 'react';
-
-import Auth from './lib/auth.js';
-import Search from './lib/search.js';
-import Details from './lib/details.js';
+import React from 'react';
+import Auth from './lib/auth.js'
+import Home from './lib/home.js';
 
 const Stack = createStackNavigator();
 
 export default function App() {
-
-  const [loading, setLoading] = useState(false);
-
   return (
-
     // Defines the navigable components of the application
     <NavigationContainer>
-
       <Stack.Navigator>
         {/* Each Stack.Screen creates a new page that we can route to, defines the Title of the Page and which component it is */}
         <Stack.Screen
-          name="Login/Sign up"
-          component={Auth} />
+          name="Auth"
+          component={Auth}
+          options={{ headerShown: false }} />
         <Stack.Screen
-          name="Search"
-          component={Search} />
-        <Stack.Screen
-          name="Details"
-          component={Details} />
-
+          name="Home"
+          component={Home}
+          options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
